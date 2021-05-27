4852923c80ad07942af51264a056cd2bbe41d1ae
YaroslavPuhalskii <netpywka14@gmail.com>
2021-05-27 14:18:48 +0300

    Add two classes about the player

---
+++ Console'sGame/Core/Actor.cs
@@ -0,0 +1,32 @@
+﻿using Console_sGame.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLNET;
+using RogueSharp;
+
+namespace Console_sGame.Core
+{
+    public class Actor : IActor, IDrawable
+    {
+        public string Name { get; set; }
+        public int Awareness { get; set; }
+
+        public RLColor Color { get; set; }
+        public char Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw (RLConsole console, IMap map)
+        {
+            if (!map.GetCell(X, Y).IsExplored)
+                return;
+            if (map.IsInFov(X, Y))
+                console.Set(X, Y, Color, Colors.FloorBackgroundFov, Symbol);
+            else
+                console.Set(X, Y, Color, Colors.FloorBackground, '.');
+        }
+    }
+}
